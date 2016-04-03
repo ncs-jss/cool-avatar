c1309d9ee91cf452ea62db088fbcd6dc1709a14e
deshraj <deshrajdry@gmail.com>
2016-04-04 01:31:02 +0530

    db name changed

--- avatar/settings.py
+++ avatar/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'avatar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'avatar'),
     }
 }
 
