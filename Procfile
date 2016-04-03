8d3dcbd21ef051b6df4661bd5e4479e10a78c5f2
deshraj <deshrajdry@gmail.com>
2016-04-04 01:15:52 +0530

    Procfile added for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn avatar.wsgi --log-file -
--- avatar/settings.py
+++ avatar/settings.py
@@ -118,8 +118,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = 'staticfiles'
 
 ACCOUNT_LOGOUT_ON_GET = True
+
+STATICFILES_DIRS = (
+                os.path.join(BASE_DIR, 'static'),
+)
