56acda27ed6397b5c609e70cc1ee7fffe9a5e7de
deshraj <deshrajdry@gmail.com>
2016-04-04 17:54:40 +0530

    media url changed

--- avatar/urls.py
+++ avatar/urls.py
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
